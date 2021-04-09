853954d62ad186235f8cb783a397363df73b5dea
Alan <a.figs@protonmail.com>
2021-04-08 22:46:16 -0700

    refactoring code

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::env;
 use std::fs;
 use std::process;
+use std::error::Error;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -12,11 +13,20 @@ fn main() {
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
+    
+    if let Err(e) = run(config) {
+        println!("Application error {}", e);
 
-    let contents = fs::read_to_string(config.filename)
-        .expect("Something went wrong reading the file");
+        process::exit(1);
+    }
+}
 
-    println!("The files contains:\n{}", contents);
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.filename)?;
+
+    println!("With text:\n{}", contents);
+
+    Ok(())
 }
 
 struct Config {
